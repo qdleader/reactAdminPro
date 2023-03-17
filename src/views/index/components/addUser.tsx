11ff54600721e93a3b079141d63775d7afe2e295
yangkun <yangkun@icourt.cc>
2023-03-17 16:06:12 +0800

    feat: 增加添加用户版块

---
+++ src/views/index/components/addUser.tsx
@@ -0,0 +1,74 @@
+import { Form, Input, Modal } from 'antd';
+import React from 'react'
+import { useEffect } from 'react';
+
+interface Values {
+  title: string;
+  description: string;
+  address: string;
+  hobby: string;
+}
+
+interface CollectionCreateFormProps {
+  open: boolean;
+  onCreate: (values: Values) => void;
+  onCancel: () => void;
+}
+
+const addUser = (props: CollectionCreateFormProps) => {
+  const {
+    open,
+    onCreate,
+    onCancel,
+  } = props
+  const [form] = Form.useForm();
+  useEffect(() => {
+    console.log(11,open);
+  },[open])
+  return (
+ <Modal
+      open={open}
+      title="Create a new collection"
+      okText="Create"
+      cancelText="Cancel"
+      onCancel={onCancel}
+      onOk={() => {
+        form
+          .validateFields()
+          .then((values) => {
+            form.resetFields();
+            onCreate(values);
+          })
+          .catch((info) => {
+            console.log('Validate Failed:', info);
+          });
+      }}
+    >
+      <Form
+        form={form}
+        layout="vertical"
+        name="form_in_modal"
+        initialValues={{ modifier: 'public' }}
+      >
+        <Form.Item
+          name="name"
+          label="用户名"
+          rules={[{ required: true, message: '不能为空' }]}
+        >
+          <Input />
+        </Form.Item>
+        <Form.Item name="description" label="手机号"    rules={[{ required: true, message: '不能为空' }]}>
+          <Input type="textarea" />
+        </Form.Item>
+          <Form.Item name="address" label="地址" className="collection-create-form_last-form-item"    rules={[{ required: true, message: '不能为空' }]}>
+          <Input type="textarea" />
+        </Form.Item>
+          <Form.Item name="hobby" label="爱好" className="collection-create-form_last-form-item"    rules={[{ required: true, message: '不能为空' }]}>
+          <Input type="textarea" />
+        </Form.Item>
+      </Form>
+    </Modal>
+  )
+}
+
+export default  addUser
--- src/views/index/index.tsx
+++ src/views/index/index.tsx
@@ -1,7 +1,25 @@
-import React from 'react'
+import { Button } from 'antd'
+import React, { useState } from 'react'
+import AddUser from './components/addUser';
 
 export default function index() {
+    const [open, setOpen] = useState(false);
+
+    const onCreate = (values: any) => {
+        console.log('Received values of form: ', values);
+        setOpen(false);
+    };
+
     return (
-        <div>index11111</div>
+        <div>
+            <Button type="primary" onClick={() => {
+                setOpen(true);
+                }}>添加用户</Button>
+            <AddUser   open={open}
+                onCreate={onCreate}
+                onCancel={() => {
+                setOpen(false);
+                }}/>
+        </div>
     )
 }
